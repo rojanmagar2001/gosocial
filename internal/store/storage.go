840df10d54999d16cba3b1c35b620292499edd7c
Rojan Rana Magar <rozenmagar058@gmail.com>
2025-12-16 17:17:55 +0545

    added user feed and user follow features

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -9,11 +9,13 @@ import (
 
 var (
 	ErrNotFound          = errors.New("resource not found")
+	ErrConflict          = errors.New("resource already exists")
 	QueryTimeoutDuration = time.Second * 5
 )
 
 type Storage struct {
 	Posts interface {
+		GetUserFeed(context.Context, int64, PaginatedFeedQuery) ([]PostWithMetadata, error)
 		Create(context.Context, *Post) error
 		GetByID(context.Context, int64) (*Post, error)
 		Delete(context.Context, int64) error
@@ -22,18 +24,25 @@ type Storage struct {
 
 	Users interface {
 		Create(context.Context, *User) error
+		GetByID(context.Context, int64) (*User, error)
 	}
 
 	Comments interface {
 		GetByPostID(context.Context, int64) ([]Comment, error)
 		Create(context.Context, *Comment) error
 	}
+
+	Followers interface {
+		Follow(context.Context, int64, int64) error
+		Unfollow(context.Context, int64, int64) error
+	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts:    &PostStore{db},
-		Users:    &UserStore{db},
-		Comments: &CommentStore{db},
+		Posts:     &PostStore{db},
+		Users:     &UserStore{db},
+		Comments:  &CommentStore{db},
+		Followers: &FollowerStore{db},
 	}
 }
